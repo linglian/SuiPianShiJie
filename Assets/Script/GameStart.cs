5aa05eeca6eddac8fae7957dbd0e1decd85cf047
linglian <linglian@aliyun.com>
2016-10-17 20:41:46 +0800

    匹配真的做完了！！

---
+++ Assets/Script/GameStart.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStart : MonoBehaviour {
+	Canvas gameCanvas;
+	// Use this for initialization
+	void Start () {
+		gameCanvas = this.transform.Find ("GameCanvas").GetComponent<Canvas> ();
+		gameCanvas.gameObject.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Script/SpriteChangePixel.cs
+++ Assets/Script/SpriteChangePixel.cs
@@ -1,17 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraChange : MonoBehaviour {
+public class SpriteChangePixel : MonoBehaviour {
 	public GameObject ca;
+	public float width;
+	public float height;
 	// Use this for initialization
 	void Start () {
 		Camera cam = ca.GetComponent<Camera>();
 		float ort = cam.orthographicSize;
 		float wh = (float)Screen.width / (float)Screen.height;
-		float normalWidth = ort * 2 * (640f / 960f);
+		float normalWidth = ort * 2 * (width / height);
 		float newHeight = normalWidth / wh;
-		cam.orthographicSize = newHeight/2.0f;
-		Debug.Log ("cam.orthographicSize = " +cam.orthographicSize);
-
+		float ooW = newHeight / 2.0f / ort;
+		Vector3 pos = transform.lossyScale;
+		pos.x /= ooW;
+		transform.localScale = pos;
 	}
 }
