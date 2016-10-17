604755cb2868f6ef973ebfad0c2de16c0997cc32
linglian <linglian@aliyun.com>
2016-10-17 10:20:30 +0800

    屏幕适配弄好了

---
+++ Assets/Script/CameraChange.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraChange : MonoBehaviour {
+	public GameObject ca;
+	// Use this for initialization
+	void Start () {
+		Camera cam = ca.GetComponent<Camera>();
+		float ort = cam.orthographicSize;
+		float wh = (float)Screen.width / (float)Screen.height;
+		float normalWidth = ort * 2 * (640f / 960f);
+		float newHeight = normalWidth / wh;
+		cam.orthographicSize = newHeight/2.0f;
+		Debug.Log ("cam.orthographicSize = " +cam.orthographicSize);
+
+	}
+}
