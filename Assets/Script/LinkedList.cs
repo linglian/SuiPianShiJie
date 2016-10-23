744b8728f996fb66825341503063df5d14a06505
linglian <linglian@aliyun.com>
2016-10-23 19:51:34 +0800

    解决了按键问题，真实运行全用虚拟按键来执行

---
+++ Assets/Script/LinkedList.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+
+public class LinkedList<DataType>
+{
+	public int count;
+	private Node<DataType> head;
+
+	public LinkedList(){
+		head = null;
+		count = 0;
+	}
+
+	public DataType pop(){
+		Node<DataType> temp = head;
+		head = head.next;
+		count--;
+		return temp.data;
+	}
+
+	public void add(DataType node){
+		if (head == null) {
+			head = new Node<DataType> (node);
+		} else {
+			Node<DataType> tempNode = head;
+			while(tempNode.next!=null)
+				tempNode = tempNode.next;
+			tempNode.next = new Node<DataType> (node);
+		}
+		count++;
+	}
+	public DataType get(int index){
+		Node<DataType> temp = head;
+		int n = 0;
+		while (n != index&&temp.next!=null) {
+			temp = temp.next;
+		}
+		return temp.data;
+	}
+
+	class Node<NodeDataType>{
+		public NodeDataType data;
+		public Node<NodeDataType> next;
+
+		public Node(NodeDataType data){
+			this.data = data;
+			next = null;
+		}
+	}
+}
+
--- Assets/Script/MainGame.cs
+++ Assets/Script/MainGame.cs
@@ -3,14 +3,47 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class MainGame : MonoBehaviour {
+	
+	//地图相关
 	public GameObject floor;
 	public GameObject XuanXiang;
 	public int Width = 50;
 	public int Height = 50;
-	Queue buttonQueue;
-	// Use this for initialization
+
+	//选项相关
+	private LinkedList<GameObject> buttonList;
+	private int choseXuanXiang;//当前选择的选项,默认为0
+
+	//移动相关
+	public GameObject npc;
+	private Animator ani;
+	private Move move;
+	private float dur = 0.0f;
+	private int mode = 0;
+
+	//静态常量
+	public const int MODE_MOVE = 0;
+	public const int MODE_FIGHT = 1;
+	public const int MODE_XUANXIANG = 2;
+	public const int MODE_DUIHUA = 3;
+
+	public const int KEY_UP = 0;
+	public const int KEY_DOWN = 1;
+	public const int KEY_LEFT = 2;
+	public const int KEY_RIGHT = 3;
+	public const int KEY_YES = 4;
+	public const int KEY_NO = 5;
+
+	/************************
+	 *不对外开放的函数
+	 *
+	 *
+	 ************************/
+
+	//初始化函数，系统自动调用
 	void Start () {
-		buttonQueue = new Queue ();
+		buttonList = new LinkedList<GameObject> ();
+		//初始化地图信息
 		GameObject obj;
 		Vector3 pos = Vector3.zero;
 		for (int i = -Width/2; i < Width/2; i++) {
@@ -21,17 +54,173 @@ public class MainGame : MonoBehaviour {
 				obj.transform.SetParent (this.transform.Find ("GameFloor").transform);
 			}
 		}
+
+		//初始化移动相关
+		ani = npc.transform.GetComponent<Animator> ();
+		move =npc.transform.GetComponent<Move> ();
 	}
-	public void DestroyButton(){
-		while (buttonQueue.Count != 0) {
-			Destroy ((GameObject)buttonQueue.Dequeue ());
+
+	//更新函数
+	void FixedUpdate () {
+		/*此为电脑键盘输入，测试用，实际运行时请注释掉此处
+		if (Input.anyKey) {
+			if(Input.GetKeyDown(KeyCode.W)){
+				setKeyDown(KEY_UP);
+			} else if(Input.GetKey(KeyCode.S)){
+				setKeyDown(KEY_DOWN);
+			} else if(Input.GetKey(KeyCode.D)){
+				setKeyDown(KEY_RIGHT);
+			} else if(Input.GetKey(KeyCode.A)){
+				setKeyDown(KEY_LEFT);
+			}
+		}
+		*/
+	}
+
+
+	//释放之前的按键
+	private void destroyButton(){
+		while (buttonList.count != 0) {
+			Destroy (buttonList.pop ());
 		}
 	}
+
+	//移动所控制的npc
+	private void moveNpc(int mode){
+		switch(mode){
+		case Move.MOVEMODE_DOWN:
+			ani.CrossFade("Down",dur);
+			move.setMoveMode (Move.MOVEMODE_DOWN);
+			break;
+		case Move.MOVEMODE_UP:
+			ani.CrossFade("Up",dur);
+			move.setMoveMode (Move.MOVEMODE_UP);
+			break;
+		case Move.MOVEMODE_LEFT:
+			ani.CrossFade("Left",dur);
+			move.setMoveMode (Move.MOVEMODE_LEFT);
+			break;
+		case Move.MOVEMODE_RIGHT:
+			ani.CrossFade("Right",dur);
+			move.setMoveMode (Move.MOVEMODE_RIGHT);
+			break;
+		}
+	}
+
+	//方向键上的事件
+	private void KeyUpEvent(){
+		switch(this.mode){
+		case MODE_MOVE:
+			moveNpc (Move.MOVEMODE_UP);
+			break;
+		case MODE_XUANXIANG:
+			if (choseXuanXiang != 0) {
+				buttonList.get (--choseXuanXiang).GetComponent<Button>().Select ();
+			}
+			break;
+		}
+	}
+
+	//方向键下的事件
+	private void KeyDownEvent(){
+		switch(this.mode){
+		case MODE_MOVE:
+			moveNpc (Move.MOVEMODE_DOWN);
+			break;
+		case MODE_XUANXIANG:
+			if (choseXuanXiang != buttonList.count) {
+				buttonList.get (++choseXuanXiang).GetComponent<Button>().Select ();
+			}
+			break;
+		}
+	}
+
+	//方向键左的事件
+	private void KeyLeftEvent(){
+		switch(this.mode){
+		case MODE_MOVE:
+			moveNpc (Move.MOVEMODE_LEFT);
+			break;
+		case MODE_XUANXIANG:
+			backMove ();
+			break;
+		}
+	}
+
+	//方向键右的事件
+	private void KeyRightEvent(){
+		switch(this.mode){
+		case MODE_MOVE:
+			moveNpc (Move.MOVEMODE_RIGHT);
+			break;
+		case MODE_XUANXIANG:
+			backMove ();
+			break;
+		}
+	}
+
+	//YES按键的事件
+	private void KeyYesEvent(){
+		switch(this.mode){
+		case MODE_MOVE:
+			break;
+		case MODE_XUANXIANG:
+			buttonList.get (choseXuanXiang).GetComponent<XuanXiangEvent> ().OnClickListener ();
+			buttonList.get (choseXuanXiang).GetComponent<Button> ().Select ();
+			break;
+		}
+	}
+
+	//NO按键的事件
+	private void KeyNoEvent(){
+		switch(this.mode){
+		case MODE_MOVE:
+			break;
+		case MODE_XUANXIANG:
+			backMove ();
+			break;
+		}
+	}
+
+	/************************
+	 *public的接口
+	 *
+	 *
+	 ************************/
+	//按键接口
+	public void backMove(){
+		destroyButton ();
+		this.mode = MODE_MOVE;
+	}
+	public void setKeyDown(int key){
+		switch(key){
+		case KEY_UP:
+			KeyUpEvent ();
+			break;
+		case KEY_DOWN:
+			KeyDownEvent ();
+			break;
+		case KEY_RIGHT:
+			KeyRightEvent ();
+			break;
+		case KEY_LEFT:
+			KeyLeftEvent ();
+			break;
+		case KEY_YES:
+			KeyYesEvent ();
+			break;
+		case KEY_NO:
+			KeyNoEvent ();
+			break;
+		}
+	}
+
+	//放置选项按键，传入的参数是GameEvent数组，需要继承GameEvent类来进行开发
 	public void setButton(GameEvent []gameEvent){
 		GameObject obj;
 		Vector3 pos;
 		XuanXiangEvent xxEvent;
-		DestroyButton ();
+		destroyButton ();
 		for (int i = 0; i < gameEvent.Length; i++) {
 			pos = Vector3.zero;
 			obj = (GameObject)Instantiate (XuanXiang, pos, Quaternion.identity);
@@ -47,11 +236,18 @@ public class MainGame : MonoBehaviour {
 			text.text = (i+1)+","+gameEvent[i].text;
 			xxEvent = obj.GetComponent<XuanXiangEvent> ();
 			xxEvent.setGameEvent (gameEvent [i]);
-			buttonQueue.Enqueue (obj);
+			buttonList.add(obj);
 		}
+		this.mode = MODE_XUANXIANG;
+		buttonList.get (0).GetComponent<Button> ().Select ();
+		choseXuanXiang = 0;
 	}
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void setMode(int mode){
+		this.mode = mode;
 	}
+	public int getMode(){
+		return mode;
+	}
+
 }
